4bedb39b83e748e94b9d350e75d09b33971772ec
Pornsuk <prasom.t@gmail.com>
2015-05-24 13:34:25 +0700

    first commit

---
+++ MyFirstWcfRestful/ProductRESTService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyFirstWcfRestful
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ProductRESTService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ProductRESTService.svc or ProductRESTService.svc.cs at the Solution Explorer and start debugging.
+    public class ProductRESTService : IProductRESTService
+    {
+        public List<Product> GetProductList()
+        {
+            return Products.Instant.ProductList;
+        }
+    }
+}
---
+++ MyFirstWcfRestful/Products.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace MyFirstWcfRestful
+{
+    [DataContract]
+    public class Product
+    {
+
+        [DataMember]
+        public int ProductId { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Categoty { get; set; }
+        [DataMember]
+        public int Price { get; set; }
+
+        
+    }
+    public partial class Products
+    {
+        private static readonly Products _instant = new Products();
+        private Products() { }
+        public static Products Instant
+        {
+            get { return _instant; }
+        }
+
+        public List<Product> ProductList
+        {
+            get { return _products;}
+        }
+
+        private List<Product> _products = new List<Product>()
+        {
+            new Product(){ProductId = 1,Name="Nokia",Categoty = "Phone",Price = 20000},
+            new Product(){ProductId = 2,Name="Samsung",Categoty = "Phone",Price = 18000},
+            new Product(){ProductId = 3,Name="Apple",Categoty = "Phone",Price = 25000},
+            new Product(){ProductId = 4,Name="LG",Categoty = "Phone",Price = 20000}
+        };
+    }
+}
